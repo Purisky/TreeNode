08dfa56527ef0550f366bcb73cf907cb17fc37e7
Purisky <woolarshy@gmail.com>
2025-07-28 20:30:13 +0800

    Revert "[opt]整理文件"
    This reverts commit 80be8c9d1c6efb1b9bc2577e9f93e2859cd8beb8.

--- Editor/TreeNode/Format.cs
+++ Editor/TreeNode/Format.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Xml;
-using TreeNode.Runtime;
+using System;
 using TreeNode.Utility;
 using UnityEditor.PackageManager.UI;
 using UnityEngine;
-using UnityEngine.UIElements;
+using System.Linq;
+using TreeNode.Runtime;
 
 namespace TreeNode.Editor
 {
-    public partial class TreeNodeGraphView//Format
+    public partial class TreeNodeGraphView
     {
         const int X_SPACE = 30;
         const int Y_SPACE = 10;
@@ -134,41 +133,6 @@ namespace TreeNode.Editor
                     FormatNode(child, visitedNodes);
                 }
             }
-        }
-        private void FormatAllNodes(DropdownMenuAction a)
-        {
-            // 开始批量操作记录
-            var batchCommand = Window.History.BeginBatch();
-
-            // 记录所有节点的位置变化
-            var positionCommands = new List<PropertyChangeCommand>();
-            foreach (var viewNode in ViewNodes)
-            {
-                var oldPosition = viewNode.Data.Position;
-                var posCommand = new PropertyChangeCommand(
-                    viewNode.Data,
-                    "Position",
-                    oldPosition,
-                    oldPosition // 这里先用旧值，FormatNodes后会被实际的新值替换
-                );
-                positionCommands.Add(posCommand);
-                batchCommand.AddCommand(posCommand);
-            }
-
-            // 调用实际的格式化方法
-            FormatNodes();
-
-            // 更新批量命令中的新位置值
-            for (int i = 0; i < Math.Min(positionCommands.Count, ViewNodes.Count); i++)
-            {
-                // 通过反射更新newValue
-                positionCommands[i].GetType()
-                    .GetField("newValue", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                    ?.SetValue(positionCommands[i], ViewNodes[i].Data.Position);
-            }
-
-            // 结束批量操作记录
-            Window.History.EndBatch(batchCommand);
         }
     }
 }
