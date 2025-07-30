6c1336dfe5fbc0dff21a52ca80eeee0c75193931
Purisky <woolarshy@gmail.com>
2025-07-30 14:21:39 +0800

    Update PAPath.cs

--- Runtime/Property/PAPath.cs
+++ Runtime/Property/PAPath.cs
@@ -2,14 +2,21 @@
 
 namespace TreeNode.Runtime
 {
-
-
-
     public struct PAPath
     {
-        
+        public PAPart[] Parts;
 
 
+        public PAPath(string path)
+        {
+            Parts = new PAPart[0];
+        }
+    }
+    public struct PAPart
+    {
+        public int Index;
+        public string Name;
 
+        public readonly bool IsIndex => Name == null;
     }
 }
