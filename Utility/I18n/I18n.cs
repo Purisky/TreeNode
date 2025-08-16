9e014593816ab87ceac3f7d6de483ed346dd2ed1
Purisky <woolarshy@gmail.com>
2025-08-16 15:01:52 +0800

    Update I18n.cs

--- Utility/I18n/I18n.cs
+++ Utility/I18n/I18n.cs
@@ -2,8 +2,6 @@
 
 #if TREENODE_CHINESE
 using Current = TreeNode.Utility.Chinese;
-#elif TREENODE_FRENCH
-using Current = TreeNode.Utility.French;
 #else
 using Current = TreeNode.Utility.English;
 #endif
